793c28a1c70910172270d809a40dab2c1c7476a9
Tom Vamvanij <tom.vamvanij@gmail.com>
2016-07-13 13:14:27 -0700

    explantory comment

--- chlamydia.js
+++ chlamydia.js
@@ -336,7 +336,7 @@ function plot(i, iOld, delay, rectDuration) { // Plotting workhorse
 
 	// Labels
 	function runNumber(selection, end, decimal) {
-		if (decimal === undefined) decimal = 1;
+		if (decimal === undefined) decimal = 1; // No d = d || 1 syntax b/c 0 is falsey
 		return function() {
 			var n = d3.interpolateNumber(selection.text().replace(/%/g, ""), end);
 			return function(t) {d3.select(this).text(n(t).toFixed(decimal)+'%');};
@@ -350,7 +350,8 @@ function plot(i, iOld, delay, rectDuration) { // Plotting workhorse
 		rLabel.transition().delay(delay).duration(rectDuration)
 			.attr('x', sR/2)
 			.attr('y', width - sV - 5)
-			.tween('text', runNumber(rLabel,currentFrame.R, (currentFrame.R < 10 && iOld !== lastFrame) ? 2 : 1));
+			.tween('text', runNumber(rLabel,currentFrame.R,
+						(currentFrame.R < 10 && iOld !== lastFrame) ? 2 : 1));
 
 		vLabel.transition().delay(delay).duration(rectDuration)
 			.attr('y', width - sV/2)
